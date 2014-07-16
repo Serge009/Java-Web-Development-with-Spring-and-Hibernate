18c6b313ed81eb66653200335c7b33f89fcea905
Serge009 <serge.naumovych@gmail.com>
2014-07-16 11:15:25 +0300

    Initial commit

---
+++ src/main/java/com/marakana/contacts/Contact.java
@@ -0,0 +1,35 @@
+package com.marakana.contacts;
+
+/**
+ * Created by Matrix on 16.07.2014.
+ */
+public class Contact {
+
+    private Long id;
+    private String name;
+    private Long addressId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(Long addressId) {
+        this.addressId = addressId;
+    }
+}
